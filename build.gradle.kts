ee0440c85a74d1098124f286db56934e498e7e99
mint <rezajaxg@gmail.com>
2024-12-16 16:43:02 +0400

    first

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+val kotlin_version: String by project
+val kotlinx_html_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.0"
+    id("io.ktor.plugin") version "3.0.2"
+}
+
+group = "baranfit.ir"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers") }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-thymeleaf-jvm")
+    implementation("io.ktor:ktor-server-html-builder-jvm")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinx_html_version")
+    implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.129-kotlin-1.4.20")
+    implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml-jvm")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/baranfit/ir/Routing.kt
@@ -0,0 +1,26 @@
+package baranfit.ir
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.thymeleaf.Thymeleaf
+import io.ktor.server.thymeleaf.ThymeleafContent
+import kotlinx.css.*
+import kotlinx.html.*
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
